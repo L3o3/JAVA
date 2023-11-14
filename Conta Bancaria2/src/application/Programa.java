10ffc5d5786f92dcac935b6871051835370d1210
L3o3 <leoedev3579@gmail.com>
2023-11-14 16:10:46 -0300

    update do projeto conta bancaria 2

--- Conta Bancaria2/src/application/Programa.java
+++ Conta Bancaria2/src/application/Programa.java
@@ -1,15 +1,39 @@
 package application;
 
+import entities.Conta;
 import entities.ContaNegocio;
+import entities.ContaPoupanca;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		ContaNegocio conta = new ContaNegocio(8008, "Leoe", 0.0, 500.0);
+		Conta cont = new Conta(1001, "Leoe", 0.0);
+		ContaNegocio contN = new ContaNegocio(1002, "Maria", 0.0, 500.0);
 		
-		System.out.println(conta.getLimiteCredito());
+		//UPCASTING
 		
+		Conta cont1 = contN;
+		Conta cont2 = new ContaNegocio(1003, "Antônio", 0.0, 200.0);
+		Conta cont3 = new ContaPoupanca(1004, "João" , 0.0, 300.0);
+		
+		//DOWNCAST
+		
+		ContaNegocio cont4 = (ContaNegocio)cont2;
+		cont4.emprestimo(100.0);
+		
+		//ContaNegocio cont5 = (ContaNegocio)cont3;
+		if (cont3 instanceof ContaNegocio) {
+			ContaNegocio cont5 = (ContaNegocio)cont3;
+			cont5.emprestimo(5000.0);
+			System.out.println("Emprestimo");
+		}
+		
+		if (cont3 instanceof ContaPoupanca) {
+			ContaPoupanca cont5 = (ContaPoupanca)cont3;
+			cont5.atualizaSaldo();
+			System.out.println("Saldo atualizado");
+		}
 	}
 
 }
---
+++ Conta Bancaria2/src/entities/ContaPoupanca.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class ContaPoupanca extends Conta{
+
+	private Double taxaJuros;
+	
+	public ContaPoupanca() {
+		super();
+	}
+
+	public ContaPoupanca(Integer conta, String titular, Double saldo, Double taxaJuros) {
+		super(conta, titular, saldo);
+		this.taxaJuros = taxaJuros;
+	}
+
+	public Double getTaxaJuros() {
+		return taxaJuros;
+	}
+
+	public void setTaxaJuros(Double taxaJuros) {
+		this.taxaJuros = taxaJuros;
+	}
+	
+	public void atualizaSaldo() {
+		saldo += saldo * taxaJuros;
+	}
+	
+}
