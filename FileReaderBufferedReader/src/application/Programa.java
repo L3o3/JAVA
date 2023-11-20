bd374e60822ef1d1b3127f1b5dff8fcb91ff2b7c
L3o3 <leoedev3579@gmail.com>
2023-11-20 15:41:30 -0300

    Aprendendo 'FileWriter''bufferedWriter'

--- FileReaderBufferedReader/src/application/Programa.java
+++ FileReaderBufferedReader/src/application/Programa.java
@@ -1,27 +1,26 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		String path = "c:\\windows\\temp\\in.txt";
+		String[] lines = new String[] {"Bom dia", "Boa tarde", "Boa noite"};
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String path = "c:\\windows\\temp\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		}
-		catch (IOException e) {
-			System.out.println("Erro: " + e.getMessage());
+			catch (IOException e){
+				e.printStackTrace();
 		}
-		
 	}
-
 }
+
