705d08c7c7d62d87443ae1daccd98b211de4058c
L3o3 <leoedev3579@gmail.com>
2023-12-12 14:38:50 -0300

    Comparator objeto de expressão lambda com chaves

--- Programação Funcional - Comparator/src/application/Program.java
+++ Programação Funcional - Comparator/src/application/Program.java
@@ -5,19 +5,18 @@ import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
 
 		list.sort(comp);
