f12c8c63755be285e550624e76c75b6413245209
L3o3 <leoedev3579@gmail.com>
2023-12-22 14:58:26 -0300

    Reference method com método não estático

--- Consumer/src/application/Program.java
+++ Consumer/src/application/Program.java
@@ -2,11 +2,9 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import entities.Product;
 
 public class Program {
-
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
 		list.add(new Product("Tv", 900.00));
@@ -14,7 +12,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::staticPriceUpdate);
+		list.forEach(Product::nonStaticPriceUpdate);
 
 		list.forEach(System.out::println);
 	}
