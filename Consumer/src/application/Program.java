ccbe1146004fdc0278f312656ae1ce64fcbf3dcb
L3o3 <leoedev3579@gmail.com>
2023-12-22 15:13:42 -0300

    Expressão lambda inline

--- Consumer/src/application/Program.java
+++ Consumer/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
 import entities.Product;
 
 public class Program {
@@ -16,11 +15,7 @@ public class Program {
 
 		double factor = 1.1;
 
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-		};
-
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
 		list.forEach(System.out::println);
 	}
