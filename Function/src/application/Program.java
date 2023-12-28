cf4d2dbee7e970a11ef01b7c7c0d1b516f3d34fe
L3o3 <leoedev3579@gmail.com>
2023-12-27 21:22:14 -0300

    Reference method com método estático

--- Function/src/application/Program.java
+++ Function/src/application/Program.java
@@ -2,7 +2,8 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
+
 import entities.Product;
 
 public class Program {
@@ -13,8 +14,14 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		double factor = 1.1;
 
-		names.forEach(System.out::println);
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+
+		list.forEach(cons);
+
+		list.forEach(System.out::println);
 	}
 }
